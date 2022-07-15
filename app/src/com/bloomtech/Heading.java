1485282722a07835ca7b10ec8f9179b6446a1740
jcassidy91 <jamie@Lambda-School-Jamies-MacBook-Pro.local>
2022-07-15 15:01:01 -0700

    initial

---
+++ app/src/com/bloomtech/Heading.java
@@ -0,0 +1,35 @@
+package com.bloomtech;
+
+public class Heading {
+  private String direction;
+  private float time;
+  private float speedInKnots;
+
+  public Heading(String direction, float time, float speedInKnots) {
+    this.direction = direction;
+    this.time = time;
+    this.speedInKnots = speedInKnots;
+  }
+
+  public String getDirection() {
+    // TODO: implement
+  }
+
+  public float getTime() {
+    // TODO: implement
+  }
+
+  public float getSpeedInKnots() {
+    // TODO: implement
+  }
+
+  @Override
+  public String toString() {
+    // TODO: implement
+  }
+
+  @Override
+  public boolean equals(Object otherHeading) {
+    // TODO: implement
+  }
+}
